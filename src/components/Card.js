343ffe743076b9644e78d3c2dacb432104a2f8f8
Mahendra Kumar <mahendra@Mahendras-MacBook-Pro.local>
2018-06-12 16:47:41 +0200

    initial commit

---
+++ src/components/Card.js
@@ -0,0 +1,22 @@
+import React from 'react';
+import { Link } from 'react-router-dom';
+import './Card.css';
+
+const Card = (props) =>  (
+  
+    <div className={props.card.animation}>
+     <Link to={`/products/${props.card.id}`}>
+        <div className="front">
+          <img src="juice.jpg" alt="Avatar" className="card-image" />
+          <div className="container">
+            <h3>{props.card.title}</h3>
+            <h3><span className="price"> ${props.card.price}</span></h3> 
+            <p>{props.card.description}</p>
+          </div>
+        </div>
+      </Link>
+    </div>
+
+);
+
+export default Card;
---
+++ src/components/NotFound.js
@@ -0,0 +1,9 @@
+import React from 'react';
+
+const NotFound = () => {
+    return (
+        <h1 style={{"color":"white"}}> 404 Page not Found.......Go Back!!!</h1>
+    )
+}
+ 
+export default NotFound;
